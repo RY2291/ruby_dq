b87be9eeb2a9764a938fb111264f395d965653e1
RY2291 <prgr2498@gmail.com>
2022-01-25 12:07:57 +0900

    [fin]brave_attack

--- main.rb
+++ main.rb
@@ -2,19 +2,44 @@ class Brave
   attr_reader :name, :defense, :offense
   attr_accessor :hp
 
+  SP_ATTACK_CONST = 1.5
+
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
   end
+
+  def attack(monster)
+    p "#{@name}の攻撃"
+
+    attack_num = rand(4)
+
+    if attack_num == 0
+      puts "必殺攻撃"
+      damage = calculate_sp_attack - monster.defense
+    else
+      puts "通常の攻撃"
+      damage = @offense -monster.defense
+    end
+
+    monster.hp -= damage
+    p "#{monster.name}は#{damage}のダメージを受けた"
+    p "#{monster.name}のHPは#{monster.hp}だ"
+  end
+
+  def calculate_sp_attack
+    @offense * SP_ATTACK_CONST
+  end
+
 end
 
 class Monster < Brave
 end
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
-monster1 = Monster.new(name: "スライム", hp: 50, offense: 10, defense: 25)
+monster1 = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 
 p "NAME:#{brave.name}"
@@ -23,8 +48,4 @@ p "OFFENSE:#{brave.offense}"
 p "DEFFENSE:#{brave.defense}"
 
 
-monster1.offense
-brave.hp -= 10
-
-p "#{monster1.name}は#{monster1.offense}のダメージを与えた"
-p "#{brave.name}はダメージを受けた!　残りHPは#{brave.hp}だ"
+brave.attack(monster1)
